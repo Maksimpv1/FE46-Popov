ab31fbb53d502aa08a42ec94b0038c06b595ef4b
Maksim noyt <maksimpv1@bk.ru>
2023-02-27 17:59:44 +0300

    din't see)

--- Lesson-add/index.js
+++ Lesson-add/index.js
@@ -1,39 +1,42 @@
 const video = document.querySelector('#videoPlayer');
 const volumeV = document.getElementById('volume');
-document.querySelector('#volume-btn').onclick= showVolume;
+// document.querySelector('#volume-btn').onclick= showVolume;
+// document.querySelector('#play').onclick= playFunc;
+// document.querySelector('#stop').onclick= stopFunc;
+// document.querySelector('#pause').onclick= pauseFunc;
+// document.querySelector('#volume').oninput = videoVolume;
 
-document.querySelector('#play').onclick= playFunc;
-document.querySelector('#stop').onclick= stopFunc;
-document.querySelector('#pause').onclick= pauseFunc;
-document.querySelector('#volume').oninput = videoVolume;
+const clickShowVolume = document.querySelector('#volume-btn');
+const clickPlay = document.querySelector('#play');
+const clickStop = document.querySelector('#stop');
+const clickPause = document.querySelector('#pause');
 
-function showVolume(){
-    if(volumeV.style.display == 'none'){
-    volumeV.style.display =  'inline';
-}else{
-        volumeV.style.display = 'none';
-    }
-}
-
-function playFunc(){
-    video.play();
-
-};
-
-function pauseFunc(){
-    video.pause();
-    video.currentTime = 0;
-
-};
-
-function stopFunc(){
-    video.pause();
-
-};
+// function showVolume(){
+//     if(volumeV.style.display == 'none'){
+//     volumeV.style.display =  'inline';
+// }else{
+//         volumeV.style.display = 'none';
+//     }
+// }
 
 
 
-function videoVolume() {
-    let sec = this.value;
-    video.volume = sec / 100;
-}; 
+
+
+// function pauseFunc(){
+//     video.pause();
+//     video.currentTime = 0;
+
+// };
+
+// function stopFunc(){
+//     video.pause();
+
+// };
+
+
+
+// function videoVolume() {
+//     let sec = this.value;
+//     video.volume = sec / 100;
+// }; 
